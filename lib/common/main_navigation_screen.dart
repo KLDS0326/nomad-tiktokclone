14db62430b158a44be63cd1ae1771fb798575977
myeongpil <audvlf92@naver.com>
2023-07-22 19:21:21 +0900

    Router Part.1 && common 폴더추가

---
+++ lib/common/main_navigation_screen.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:kakaomap_webview/kakaomap_webview.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/discover/discover_screen.dart';
+import 'package:tiktok_clone/features/inbox/inbox_screen.dart';
+import 'package:tiktok_clone/features/kakao_map/kakao_map_screen.dart';
+import 'package:tiktok_clone/common/widgets/nav_tab.dart';
+import 'package:tiktok_clone/common/widgets/post_video_button.dart';
+import 'package:tiktok_clone/features/users/user_profile_screen.dart';
+import 'package:tiktok_clone/features/videos/video_timeline_screen.dart';
+import 'package:tiktok_clone/util.dart';
+
+class MainNavigationScreen extends StatefulWidget {
+  const MainNavigationScreen({super.key});
+
+  @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  /* final screens = [
+    Container(
+      key: GlobalKey(),
+    ),
+    Container(
+      key: GlobalKey(),
+    ),
+    Container(),
+    Container(
+      key: GlobalKey(),
+    ),
+    Container(
+      key: GlobalKey(),
+    ),
+  ]; */
+
+  void _onTap(int index) {
+    setState(
+      () {
+        _selectedIndex = index;
+      },
+    );
+  }
+
+  void _onPostVideoButtonTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => Scaffold(
+          appBar: AppBar(title: const Text('Record video')),
+        ),
+        fullscreenDialog: true,
+      ),
+    );
+  }
+
+  Future<void> _openKakaoMapScreen(BuildContext context) async {
+    KakaoMapUtil util = KakaoMapUtil();
+
+    // String url = await util.getResolvedLink(
+    //     util.getKakaoMapURL(37.402056, 127.108212, name: 'Kakao 본사'));
+
+    /// This is short form of the above comment
+    String url =
+        await util.getMapScreenURL(37.402056, 127.108212, name: 'Kakao 본사');
+
+    debugPrint('url : $url');
+
+    Navigator.push(
+        context, MaterialPageRoute(builder: (_) => KakaoMapScreen(url: url)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = isDarkMode(context);
+    return Scaffold(
+      backgroundColor:
+          _selectedIndex == 0 || isDark ? Colors.black : Colors.white,
+      body: Stack(
+        children: [
+          Offstage(
+            offstage: _selectedIndex != 0,
+            child: const VideoTimelineScreen(),
+          ), // screens.elementAt(_selectedIndex), //screens[_selectedIndex],
+          Offstage(
+            offstage: _selectedIndex != 1,
+            child: const DiscoverScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 3,
+            child: const InboxScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 4,
+            child: const UserProfileScreen(
+              username: "중꺾마",
+              tab: "",
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        color: _selectedIndex == 0 || isDark ? Colors.black : Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: Sizes.size20,
+            bottom: Sizes.size32,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                  text: "리포트",
+                  isSelected: _selectedIndex == 0,
+                  icon: FontAwesomeIcons.penToSquare,
+                  selectedicon: FontAwesomeIcons.solidPenToSquare,
+                  onTap: () => _onTap(0),
+                  selectedIndex: _selectedIndex),
+              NavTab(
+                text: "Discover",
+                isSelected: _selectedIndex == 1,
+                icon: FontAwesomeIcons.ccDiscover,
+                selectedicon: FontAwesomeIcons.house,
+                onTap: () => _onTap(1),
+                selectedIndex: _selectedIndex,
+              ),
+              Gaps.h24,
+              GestureDetector(
+                onTap: _onPostVideoButtonTap,
+                child: PostVideoButton(
+                  inverted: _selectedIndex != 0,
+                ),
+              ),
+              Gaps.h24,
+              NavTab(
+                text: "선호지역",
+                isSelected: _selectedIndex == 3,
+                icon: FontAwesomeIcons.star,
+                selectedicon: FontAwesomeIcons.solidStar,
+                onTap: () => _onTap(3),
+                selectedIndex: _selectedIndex,
+              ),
+              NavTab(
+                text: "내 정보",
+                isSelected: _selectedIndex == 4,
+                icon: FontAwesomeIcons.user,
+                selectedicon: FontAwesomeIcons.solidUser,
+                onTap: () => _onTap(4),
+                selectedIndex: _selectedIndex,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/* Material 3
+return Scaffold(
+      body: screens[_selectedIndex],
+      bottomNavigationBar: NavigationBar(
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: _onTap,
+        destinations: const [
+          NavigationDestination(
+            icon: FaIcon(
+              FontAwesomeIcons.house,
+              color: Colors.teal,
+            ),
+            label: 'home',
+          ),
+          NavigationDestination(
+            icon: FaIcon(
+              FontAwesomeIcons.magnifyingGlass,
+              color: Colors.amber,
+            ),
+            label: 'Search',
+          ),
+        ],
+      ),
+    );
+   */
+
+/* Material 2
+bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _selectedIndex,
+        onTap: _onTap,
+        //selectedItemColor: Theme.of(context).primaryColor,
+        items: const [
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: "WHar are you",
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: "what are you",
+            backgroundColor: Colors.teal,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: "what are you",
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: "what are you",
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: "what are you",
+            backgroundColor: Colors.pink,
+          ),
+        ],
+      ), */
